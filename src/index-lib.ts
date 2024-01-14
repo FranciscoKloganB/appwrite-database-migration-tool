69c06deb1e8d337fe32865d710023eb4052305b6
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2024-01-14 23:19:42 +0000

    feat: add down one command

--- src/index-lib.ts
+++ src/index-lib.ts
@@ -2,6 +2,7 @@ export * from './lib/constants';
 export * from './lib/domain';
 export * from './lib/migrationsCreateCollection';
 export * from './lib/migrationsCreateDatabase';
+export * from './lib/migrationsDownOne';
 export * from './lib/migrationsResetDatabase';
 export * from './lib/migrationsRunSequence';
 export * from './lib/repositories';
