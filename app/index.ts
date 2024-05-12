bf2023c7732ed6f275934e8e2a1df8869a022f05
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2024-05-12 13:06:34 +0100

    chore: run biome

--- app/index.ts
+++ app/index.ts
@@ -11,12 +11,15 @@
 /**  ModuleJS aliased import */
 
 /**  ModuleJS Destruct MJS import  */
-import * as APKG from '@franciscokloganb/appwrite-database-migration-tool';
-import { createMigrationCollection } from '@franciscokloganb/appwrite-database-migration-tool';
+import * as APKG from '@franciscokloganb/appwrite-database-migration-tool'
+import { createMigrationCollection } from '@franciscokloganb/appwrite-database-migration-tool'
 
 console.log(
   'MJS named import as aliased object:',
   typeof APKG.createMigrationCollection === 'function',
-);
+)
 
-console.log('MJS named import destruct:', typeof createMigrationCollection === 'function');
+console.log(
+  'MJS named import destruct:',
+  typeof createMigrationCollection === 'function',
+)
